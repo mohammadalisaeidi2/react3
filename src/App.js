7526dada0e4271ec9ba3f6d73850dfa4bf983bcb
Mohammadali <mohammadalisaeidi@yahoo.com>
2022-11-14 19:43:08 +0330

    Card shuffling and first view of cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
+const cardImages = [
+  {"src" : "/img/sword-1.png"},
+  {"src" : "/img/helmet-1.png"},
+  {"src" : "/img/potion-1.png"},
+  {"src" : "/img/ring-1.png"},
+  {"src" : "/img/scroll-1.png"},
+  {"src" : "/img/shield-1.png"}
+]
+
 function App() {
+  const [cards, serCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+  //shuffle card function
+  const shuffleCards = () =>{
+    const shuffeledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({...card , id:Math.random() }))
+    serCards(shuffeledCards)
+    setTurns(0)
+  }
+  
+  console.log(cards, turns)
+
+
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards }>New Game</button>
+
+      <div className='card-grid'>
+        {cards.map( card => (
+          <div className='card' key={card.id}>
+            <div>
+              <img className='front' src={card.src} alt='card front' ></img>
+              <img className='back' src='/img/cover.png' alt='card back'></img>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
